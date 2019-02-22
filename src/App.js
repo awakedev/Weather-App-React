1ff0c3473e9420a4533cb0b7bf99aa2b51fc29aa
Nathan C <32592701+awakedev@users.noreply.github.com>
2019-02-22 21:36:06 +0000

    api

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
+const API_KEY =  "b4810559bdb5e195b5bb6d3bfd7d8da5";
 class App extends React.Component {
+  getWeather = async () => {
+    const api_call = await fetch(`api.openweathermap.org/data/2.5/weather?q=Belfast,Uk&appid=${API_KEY}&units=metric`);
+
+    const data = await api_call.json();
+  
+    
+  }
+
   render() {
     return (
       <div>
